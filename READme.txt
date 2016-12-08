c0f1f3e99b719f45c6e2bdaf6b39feedc7614e4c
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2016-12-07 21:21:42 -0800

    working xml data

--- READme.txt
+++ READme.txt
@@ -1,1 +1,14 @@
 needed npm install --save-dev babel-core -DONE
+
+creates new branch and name
+git checkout -b feat/xml
+
+git initiats github
+checkout -b creates new branch
+feat/xml is the name
+
+git push origin master
+
+push send
+origin where
+name(master, feat/xml) where 
--- server/server.js
+++ server/server.js
@@ -10,24 +10,32 @@ var fs = require('fs');
 // import xml from './parseme.xml'
 xml2js = require('xml2js');
 
-
+//app.use(static, __dirname + 'client')
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('*', (req, res)=>{
 
-    console.log('success')
+  request('http://feeds.feedburner.com/thetimferrissshow', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body, ' bodyyyyy') 
+  }
+  console.log(body, ' log line 58') 
+  res.send(body)
+})
 
-var parser = new xml2js.Parser();
-fs.readFile(__dirname + '/parseme.xml', function(err, data) {
-    parser.parseString(data, function (err, result) {
-       // console.dir(result);
-        res.send(result)
-        console.log(result);
-        console.log('Done');
-    });
-});
+//     console.log('success')
+
+// var parser = new xml2js.Parser();
+// fs.readFile(__dirname + '/parseme.xml', function(err, data) {
+//     parser.parseString(data, function (err, result) {
+//        // console.dir(result);
+//         res.send(result)
+//         console.log(result);
+//         console.log('Done');
+//     });
+// });
 
 //res.send('yo')
 
