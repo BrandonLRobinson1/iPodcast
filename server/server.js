0b5ab5a4fd9c81bcef1fb3ff6fd7df5ae37f6ad1
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2016-12-07 22:32:46 -0800

    working!!

--- server/server.js
+++ server/server.js
@@ -3,44 +3,48 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 var fs = require('fs');
+var xml2js = require('xml2js')
+var $ = require('jquery')
 // var fs
-//eval(require('fs').readFileSync('./xml.js', 'utf8')); 
+//eval(require('fs').readFileSync('./xml.js', 'utf8'));
 //var xml = require('./parseme.xml')
 //var xml = require('./xml')
 // import xml from './parseme.xml'
-xml2js = require('xml2js');
-
-//app.use(static, __dirname + 'client')
-
-app.use(bodyParser.json()); // for parsing application/json
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE');
+  next();
+});
+app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.get('*', (req, res)=>{
-
-  request('http://feeds.feedburner.com/thetimferrissshow', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body, ' bodyyyyy') 
+app.get('/search', (req, res)=>{
+  console.log('expect req.body to equal obj with property searchQuery and url',req.body)
+  console.log('expect req.body.searchQuery to equal url',req.body.searchQuery)
+  var searchQuery = 'https://itunes.apple.com/search?term=the+tim+ferris+show'//pass in searchQuery
+  request(searchQuery, function (error, response, body) {//call the iTunes API
+  if (error){
+    console.log('hit error in first searchQuery when calling iTunes API',error)
   }
-  console.log(body, ' log line 58') 
-  res.send(body)
+  console.log('expect body to be jsonObject from iTunes API',JSON.stringify(typeof body))
+  var turnStringToObject = JSON.parse(body)
+  console.log('expect turnStringToObject to be an object',turnStringToObject)
+    var feedUrl = turnStringToObject.results[0].feedUrl//get feedUrl, go get XML file
+    console.log('expect feedUrl to equal link to XML file',feedUrl)
+    request(feedUrl, function(error, response, body){
+    if (error){
+      console.log('hit error in second searchQuery when fetching XML file',error)
+    }
+    console.log('expect body to be XML file',body)
+    var returnedXMLFile = body//returnedXMLfile
+    console.log('expect returnedXMLFile to be XML file',returnedXMLFile)
+    var parser = new xml2js.Parser();
+    parser.parseString(returnedXMLFile, function (err, result) {//pass it intoParseString
+    console.log('expect result to be parsedXML file as JSON',result)
+          res.send(result)
+        });
+      })
+  })
 })
-
-//     console.log('success')
-
-// var parser = new xml2js.Parser();
-// fs.readFile(__dirname + '/parseme.xml', function(err, data) {
-//     parser.parseString(data, function (err, result) {
-//        // console.dir(result);
-//         res.send(result)
-//         console.log(result);
-//         console.log('Done');
-//     });
-// });
-
-//res.send('yo')
-
-})
-
 app.listen(3000, ()=>{
   console.log('legion of doom')
 })
